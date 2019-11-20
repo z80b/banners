2f6dfdbb58b6e1559694f5575fe02e0a5e168762
z80b <akozlov78@gmail.com>
2019-11-20 11:17:51 +0300

    LP-2208 New Year Quiz 19.11.20 11:17

--- build/css.js
+++ build/css.js
@@ -8,22 +8,24 @@ const fs           = require('fs');
 const styl = fs.readFileSync(cwd + '/' + args.input, 'utf8');
 const sourceMap = args.mode != 'production' ? { inline: true } : false;
 
-console.log('Start stylus builder...');
+function build() {
+  console.log('Start stylus builder...');
+  return stylus(styl)
+    .set('paths', [`${cwd}/src/css`, `${cwd}/src/css/copmonents`])
+    .set('filename', `${cwd}/${args.output}`)
+    .set('compress', (args.mode == 'production'))
+    .set('include css', true)
+    // .set('sourcemap', (args.mode != 'production') ? {inline: true} : false)
+    .set('sourcemap', sourceMap)
+    .define('inline-image', stylus.url())
+    .use(axis())
+    .use(autoprefixer())
+    .render((err, css) => {
+      fs.writeFile(`${cwd}/${args.output}`, css, (fsError) => {
+        if (fsError) console.log(fsError);
+        console.log('Finish stylus builder.');
+      })
+    });
+}
 
-stylus(styl)
-  .set('paths', [`${cwd}/src/css`, `${cwd}/src/css/copmonents`])
-  .set('filename', `${cwd}/${args.output}`)
-  .set('compress', (args.mode == 'production'))
-  .set('include css', true)
-  // .set('sourcemap', (args.mode != 'production') ? {inline: true} : false)
-  .set('sourcemap', sourceMap)
-  .define('inline-image', stylus.url())
-  .use(axis())
-  .use(autoprefixer())
-  .watch()
-  .render((err, css) => {
-    fs.writeFile(`${cwd}/${args.output}`, css, (fsError) => {
-      if (fsError) console.log(fsError);
-      console.log('Finish css create');
-    })
-  });
+module.exports = { build };
--- build/html.js
+++ build/html.js
@@ -1,21 +1,23 @@
 const cwd     = process.cwd();
 const args    = require('yargs').argv;
-const cheerio = require('cheerio');
 const fs      = require('fs');
 
 console.log('Start build html...');
 
-const html = fs.readFileSync(cwd + '/src/index.html', 'utf8');
-const banner = fs.readFileSync(cwd + '/src/banner.html', 'utf8');
-const $ = cheerio.load(html);
-const output = args.output || 'dist/banner.html';
+function build() {
+  const banner = fs.readFileSync(cwd + '/src/banner.html', 'utf8');
+  const styles = fs.readFileSync(cwd + '/tmp/index.css', 'utf8');
+  const script = fs.readFileSync(cwd + '/tmp/index.js', 'utf8');
+  const output = args.output || 'dist/banner.html';
+  const result = `<style>\n${styles}\n</style>\n${banner}\n<script>\n${script}\n</script>`;
 
-$('.page__banner-slot').append(banner);
 
-fs.writeFile(`${cwd}/${output}`, $.html(), {
-  encoding: "utf8",
-  flag: "a"
-}, (fsError) => {
-  if (fsError) console.log(fsError);
-  console.log('Finish build html...');
-})
+  fs.writeFile(`${cwd}/${output}`, result, {
+    encoding: "utf8",
+  }, (fsError) => {
+    if (fsError) console.log(fsError);
+    console.log('Finish build html...');
+  })
+}
+
+module.exports = { build };
