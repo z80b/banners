d92244a1801f7b0c552e4d92c26f0ffc4e5bb9bf
Kozlov Aleksandr Vladimirovich <a.kozlov@lamoda.ru>
2019-11-29 14:59:26 +0300

    autoplay

--- src/js/banner.js
+++ src/js/banner.js
@@ -1,5 +1,6 @@
 import Dom from '@js/dom.js';
 import { sendMessage } from '@js/utils/events.js';
+import { translate3d } from '@js/utils/animations.js';
 import PanoramaPick from '@js/panorama-pick.js';
 import Swiper from 'swiper';
 
@@ -9,12 +10,14 @@ class Banner extends Dom {
 
     this.slidesCount = 10;
     this.visibleSlides = 4;
-    this.init();
-    this.initSlider();
+    this.scrollDirection = 'left';
+    this.slider = this.initSlider();
+    this.initEvents();
+    console.log('constructor:', this);
     return this;
   }
 
-  init() {
+  initEvents() {
     const $imgs = this.$el.querySelectorAll('.ny-panorama__slide-img');
     $imgs.forEach(el => {
       el.addEventListener('click', () => {
@@ -24,16 +27,16 @@ class Banner extends Dom {
   }
 
   initSlider() {
-    this.slider = new Swiper('.ny-panorama__content', {
+    return new Swiper('.ny-panorama__content', {
       initialSlide: 0,
       slidesPerView: 10,
       autoHeight: true,
-      autoplay: {
-        delay: 0,
-        //reverseDirection: false,
-        // waitForTransition: false,
-      },
-      speed: 8000,
+      // autoplay: {
+      //   delay: 0,
+      //   //reverseDirection: false,
+      //   // waitForTransition: false,
+      // },
+      // speed: 8000,
       slideClass: 'ny-panorama__slide',
       wrapperClass: 'ny-panorama__track',
       freeMode: true,
@@ -53,18 +56,43 @@ class Banner extends Dom {
         }
       },
       on: {
-        init: this.initPicks.bind(this),
+        imagesReady: this.initAfterSlider.bind(this),
+        transitionEnd: this.toggleScroll.bind(this),
       }
     });
-    // this.initPicks();
-    // this.$el.addEventListener('mouseover', this.slider.autoplay.start);
-    // this.$el.addEventListener('mouseout', this.slider.autoplay.stop);
   }
 
-  initPicks() {
+  initAfterSlider() {
+    this.$track = this.$el.querySelector('.ny-panorama__track');
+    this.trackWidth
     this.$el
       .querySelectorAll('.ny-panorama__pick')
       .forEach($pick => new PanoramaPick($pick, this.$el));
+
+    this.toggleScroll();
+  }
+
+  scrollLeft() {
+    const trackWidth = this.$track.scrollWidth;
+    const viewPortWidth = this.$el.offsetWidth;
+    this.scrollDirection = 'left';
+    // console.log('scrollLeft:', trackWidth, viewPortWidth);
+    // translate3d(this.$track, `-${trackWidth - viewPortWidth}px`, 0, 0);
+    console.log('Swiper2:', this.slider.getTranslate());
+    this.slider.setTranslate(trackWidth - viewPortWidth);
+  }
+
+  scrollRight() {
+    const trackWidth = this.$track.scrollWidth;
+    const viewPortWidth = this.$el.offsetWidth;
+    this.scrollDirection = 'right';
+    this.slider.setTranslate(viewPortWidth - trackWidth);
+  }
+
+  toggleScroll() {
+    if (this.scrollDirection == 'left') {
+      this.scrollRight();
+    } else this.scrollLeft();
   }
 }
 
