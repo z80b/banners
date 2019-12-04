f058e3f50a0e24b9e397d53663c4cfe94540f21d
Kozlov Aleksandr Vladimirovich <a.kozlov@lamoda.ru>
2019-12-04 16:41:22 +0300

    LP-2216_NewYear2020_Actions_calendar (mobile version)

--- tmp/index.js
+++ tmp/index.js
@@ -10740,8 +10740,6 @@
       value: function initSlider() {
         return new Swiper('.ny-calendar__slider', {
           initialSlide: 0,
-          slidesPerView: 2,
-          spaceBetween: 30,
           watchSlidesVisibility: true,
           slideClass: 'ny-calendar-slide',
           slideActiveClass: 'ny-calendar-slide--active',
@@ -10750,10 +10748,27 @@
           slidePrevClass: 'ny-calendar-slide--prev',
           wrapperClass: 'ny-calendar__track',
           watchOverflow: true,
-          navigation: {
-            nextEl: '.ny-calendar-slider__button--next',
-            prevEl: '.ny-calendar-slider__button--prev',
-            disabledClass: 'ny-calendar-slider__button--disabled'
+          breakpoints: {
+            0: {
+              centeredSlides: true,
+              spaceBetween: 16,
+              slidesPerView: 1.5,
+              pagination: {
+                el: '.ny-calendar-slider__bullets',
+                type: 'bullets',
+                bulletClass: 'ny-calendar-slider__bullet',
+                bulletActiveClass: 'ny-calendar-slider__bullet--active'
+              }
+            },
+            960: {
+              spaceBetween: 30,
+              slidesPerView: 2,
+              navigation: {
+                nextEl: '.ny-calendar-slider__button--next',
+                prevEl: '.ny-calendar-slider__button--prev',
+                disabledClass: 'ny-calendar-slider__button--disabled'
+              }
+            }
           },
           on: {
             imagesReady: this.initAfterSlider.bind(this)
